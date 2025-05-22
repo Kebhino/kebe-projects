7b543ca1dc29efb40106573e660eef8a37354ce8
Kebhino <wrobeljakub@gmail.com>
2025-05-22 12:25:41 +0200

    adding new page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,42 @@ import GridList from "./componentsWeb/GridList";
 import Footer from "./componentsWeb/Footer";
 import ColorModeSwitch from "./hooks/useColorMode";
 import { HStack, Spacer } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import SnakeGame from "./pages/SnakeGame";
 
 function App() {
   return (
-    <>
-      <HStack>
-        <Spacer />
-        <ColorModeSwitch />
-      </HStack>
-      <GridList />
-      <Footer />
-    </>
+    <Routes>
+      {/* Strona główna */}
+      <Route
+        path="/"
+        element={
+          <>
+            <HStack>
+              <Spacer />
+              <ColorModeSwitch />
+            </HStack>
+            <GridList />
+            <Footer />
+          </>
+        }
+      />
+
+      {/* Podstrona SnakeGame */}
+      <Route
+        path="/snakegame"
+        element={
+          <>
+            <HStack>
+              <Spacer />
+              <ColorModeSwitch />
+            </HStack>
+            <SnakeGame />
+            <Footer />
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
--- src/componentsWeb/GridList.tsx
+++ src/componentsWeb/GridList.tsx
@@ -1,33 +1,39 @@
-import { Grid } from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/react";
 import ProjectCard from "./ProjectCard";
 import Cards from "../data/cards";
+import { Link } from "react-router-dom";
 
 // console.log(Cards);
 
 const GridList = () => {
   return (
-    <Grid
-      templateColumns={{
-        base: "1fr",
-        md: "repeat(2, 1fr)",
-        lg: "repeat(3, 1fr)",
-        xl: "repeat(3, 1fr)",
-      }}
-      gap={6}
-      padding={4}
-    >
-      {Cards.map((project, index) => (
-        <ProjectCard
-          title={project.title}
-          description={project.description}
-          urlImage={project.urlImage}
-          urlSite={project.urlSite}
-          git={project.git}
-          key={index}
-          id={project.id}
-        />
-      ))}
-    </Grid>
+    <>
+      <Grid
+        templateColumns={{
+          base: "1fr",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+          xl: "repeat(3, 1fr)",
+        }}
+        gap={6}
+        padding={4}
+      >
+        {Cards.map((project, index) => (
+          <ProjectCard
+            title={project.title}
+            description={project.description}
+            urlImage={project.urlImage}
+            urlSite={project.urlSite}
+            git={project.git}
+            key={index}
+            id={project.id}
+          />
+        ))}
+      </Grid>
+      <Link to="/snakegame">
+        <Box>Snake Game</Box>
+      </Link>
+    </>
   );
 };
 
