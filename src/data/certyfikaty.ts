8b20b668d3a3f93e9deff086ddf4e889677c8a6c
Kebhino <wrobeljakub@gmail.com>
2025-05-22 13:04:55 +0200

    modified:   src/data/certyfikaty.ts

--- src/data/certyfikaty.ts
+++ src/data/certyfikaty.ts
@@ -1,30 +1,30 @@
 export const CertificatesList: { name: string; url: string }[] = [
     {
-      name: "The Ultimate HTML5 & CSS3 Series",
+      name: "HTML5 & CSS3 The Ultimate Series",
       url: "https://snakegame-assets.s3.eu-north-1.amazonaws.com/Certificates/the-ultimate-html5-css3-series-part-3.pdf",
     },
     {
-      name: "The Ultimate HTML5 & CSS3 Series - Part 1",
+      name: "HTML5 & CSS3 The Ultimate Series - Part 1",
       url: "https://snakegame-assets.s3.eu-north-1.amazonaws.com/Certificates/the-ultimate-html-course-fundamentals-part-1.pdf",
     },
     {
-      name: "The Ultimate HTML5 & CSS3 Series - Part 2",
+      name: "HTML5 & CSS3 The Ultimate Series - Part 2",
       url: "https://snakegame-assets.s3.eu-north-1.amazonaws.com/Certificates/the-ultimate-html-course-advanced-concepts-part-2.pdf",
     },
     {
-      name: "The Ultimate HTML5 & CSS3 Series - Part 3",
+      name: "HTML5 & CSS3 The Ultimate Series - Part 3",
       url: "https://snakegame-assets.s3.eu-north-1.amazonaws.com/Certificates/the-ultimate-html5-css3-series-part-3.pdf",
     },
     {
-      name: "Ultimate JavaScript Part 1: Fundamentals",
+      name: "JavaScript Ultimate Part 1: Fundamentals",
       url: "https://snakegame-assets.s3.eu-north-1.amazonaws.com/Certificates/certificate-of-completion-for-javascript-basics.pdf",
     },
     {
-      name: "Ultimate JavaScript Part 2: Advanced Topcis",
+      name: "JavaScript Ultimate Part 2: Advanced Topcis",
       url: "https://snakegame-assets.s3.eu-north-1.amazonaws.com/Certificates/certificate-of-completion-for-object-oriented-programming-in-javascript.pdf",
     },
     {
-     name: "The Ultimate TypeScript Course",
+     name: "TypeScript The Ultimate Course",
      url: "https://snakegame-assets.s3.eu-north-1.amazonaws.com/Certificates/The-ultimate-type-script-course.pdf",
    },
     {
